a3d08bada56e6b6b09faef5ac8df130140e0fd98
Jesus <chusanic@gmail.com>
2023-02-20 01:58:18 -0600

    Miguellll

--- Backend/usuario/rutas/router.js
+++ Backend/usuario/rutas/router.js
@@ -1,5 +1,6 @@
 const Router = require('express');
 const router = Router();
+const jwt = require('jsonwebtoken');
 
 //middleware para validar rutas y permisos
 const {validate_session,validate_premium} = require('../middleware/validations');
@@ -15,5 +16,23 @@ router.get('/', (req, res) => {
 router.post("/test", userCtl.FTest);
 router.post("/auth", userCtl.Auth);
 router.post("/")
+
+router.post('/verify', (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, 'secretKey', (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        req.user = user;
+        res.sendStatus(200);
+    });
+});
 
 module.exports = router;
